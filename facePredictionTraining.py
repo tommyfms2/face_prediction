9af93b1bf75765acde52e4a481ba79d813b3564c
tommy <tvomxq@yahoo.co.jp>
2016-10-31 17:47:00 +0900

    forVer1.17.0_amend test data
    amend image2TrainAndTest.

--- facePredictionTraining.py
+++ facePredictionTraining.py
@@ -24,14 +24,14 @@ class Alex(chainer.Chain):
 
     def __init__(self, input_channel, n_out):
         super(Alex, self).__init__(
-            conv1=L.Convolution2D(input_channel,  32, 8, stride=4),
-            conv2=L.Convolution2D(32, 256,  5, pad=2),
-            conv3=L.Convolution2D(256, 256,  3, pad=1),
-            conv4=L.Convolution2D(256, 256,  3, pad=1),
-            conv5=L.Convolution2D(256, 32,  3, pad=1),
-            fc6=L.Linear(288, 144),
-            fc7=L.Linear(144, 50),
-            fc8=L.Linear(50, n_out),
+            conv1=L.Convolution2D(None,  32, 8, stride=4),
+            conv2=L.Convolution2D(None, 256,  5, pad=2),
+            conv3=L.Convolution2D(None, 256,  3, pad=1),
+            conv4=L.Convolution2D(None, 256,  3, pad=1),
+            conv5=L.Convolution2D(None, 32,  3, pad=1),
+            fc6=L.Linear(None, 144),
+            fc7=L.Linear(None, 50),
+            fc8=L.Linear(None, n_out),
         )
         self.train = True
 
--- image2TrainAndTest.py
+++ image2TrainAndTest.py
@@ -6,49 +6,44 @@ from chainer.datasets import tuple_dataset
 
 def image2TrainAndTest(pathsAndLabels, size=128, channels=1):
 
+    allData = []
+    for pathAndLabel in pathsAndLabels:
+        path = pathAndLabel[0]
+        label = pathAndLabel[1]
+        imagelist = glob.glob(path + "*")
+        for imgName in imagelist:
+            allData.append([imgName, label])
+    allData = np.random.permutation(allData)
+
     if channels == 1:
         imageData = []
         labelData = []
-        for pathAndLabel in pathsAndLabels:
-            path  = pathAndLabel[0]
-            label = pathAndLabel[1]
-            imagelist = glob.glob(path + "*")
-            for imgName in imagelist:
-                img = Image.open(imgName)
-                imgData = np.asarray([np.float32(img)/255.0])
-                #imgData2 = imgData.reshape(1, len(imgData)*len(imgData))[0]
-                lblData = label
-                imageData.append(imgData)
-                labelData.append(np.int32(lblData))
-
-        threshold = np.int32(len(imageData)/10*9)
+        for pathAndLabel in allData:
+            img = Image.open(pathAndLabel[0])
+            imgData = np.asarray([np.float32(img)/255.0])
+            imageData.append(imgData)
+            labelData.append(np.int32(pathAndLabel[1]))
+            
+        threshold = np.int32(len(imageData)/8*7)
         train = tuple_dataset.TupleDataset(imageData[0:threshold], labelData[0:threshold])
-        test  = tuple_dataset.TupleDataset(imageData[threshold:],  labelData[threshold:])
-
+        test = tuple_dataset.TupleDataset(imageData[threshold:], labelData[threshold:])
     else:
         imageData = []
         labelData = []
-        for pathAndLabel in pathsAndLabels:
-            path  = pathAndLabel[0]
-            label = pathAndLabel[1]
-            imagelist = glob.glob(path + "*")
-            for imgName in imagelist:
-                img = Image.open(imgName)
-                r,g,b = img.split()
-                rImgData = np.asarray(np.float32(r)/255.0)
-                gImgData = np.asarray(np.float32(g)/255.0)
-                bImgData = np.asarray(np.float32(b)/255.0)
-                imgData = np.asarray([rImgData, gImgData, bImgData])
-                #imgData = np.asarray(np.float32(img)/255.0)
-                #imgData2 = imgData.reshape(1, len(imgData)*len(imgData))[0]
-                lblData = label
-                imageData.append(imgData)
-                labelData.append(np.int32(lblData))
+        for pathAndLabel in allData:
+            img = Image.open(pathAndLabel[0])
+            r,g,b = img.split()
+            rImgData = np.asarray(np.float32(r)/255.0)
+            gImgData = np.asarray(np.float32(g)/255.0)
+            bImgData = np.asarray(np.float32(b)/255.0)
+            imgData = np.asarray([rImgData, gImgData, bImgData])
+            imageData.append(imgData)
+            labelData.append(np.int32(pathAndLabel[1]))
 
-        threshold = np.int32(len(imageData)/10*9)
+        threshold = np.int32(len(imageData)/8*7)
         train = tuple_dataset.TupleDataset(imageData[0:threshold], labelData[0:threshold])
         test  = tuple_dataset.TupleDataset(imageData[threshold:],  labelData[threshold:])
-        
+
     return train, test
 
 def getValueDataFromPath(imagePath):
@@ -72,8 +67,8 @@ def getValueDataFromImg(img):
 
 if __name__=='__main__':
     pathsAndLabels = []
-    pathsAndLabels.append(np.asarray(["./images/akimoto/output/", 0]))
-    pathsAndLabels.append(np.asarray(["./images/shiraishi/output/", 1]))
+    pathsAndLabels.append(np.asarray(["./images/akimoto/", 0]))
+    pathsAndLabels.append(np.asarray(["./images/shiraishi/", 1]))
     train, test = image2TrainAndTest(pathsAndLabels)
     print(len(train))
     print(train[10])
